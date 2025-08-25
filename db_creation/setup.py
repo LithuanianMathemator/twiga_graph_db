07dc3ae5bf98e400fc01d15dc5299df4029385b9
Jonas Lorenz <jonas@lorenz.place>
2025-08-25 17:14:22 +0200

    basic one shot creation

--- db_creation/setup.py
+++ db_creation/setup.py
@@ -2,21 +2,51 @@ import numpy as np
 from neo4j import GraphDatabase
 from dotenv import load_dotenv
 import os
+from langchain_experimental.graph_transformers import LLMGraphTransformer
+from langchain_openai import ChatOpenAI
+from langchain_neo4j import Neo4jGraph
+import asyncio
+from langchain_core.documents import Document
+import json
 
 class Graph_DB:
     
     def __init__(self, url, user, password, openai_api_key):
         
-        self.driver = GraphDatabase.driver()
-        self.driver = GraphDatabase.driver(url, auth=(user, password))
-        openai.api_key = openai_api_key
-        self.schema = self.generate_schema()
+        self.url = url
+        self.user = user
+        self.password = password
+        self.driver = GraphDatabase.driver(
+            url, 
+            auth=(user, password)
+        )
+        self.openai_api_key = openai_api_key
+        self.graph = Neo4jGraph(
+            url=url, 
+            username=user, 
+            password=password, 
+            refresh_schema=False
+        )
 
-    def add_node():
-        pass
+    async def create_graph(self, json_path: str):
+        
+        llm = ChatOpenAI(temperature=1, model_name="gpt-4.1-nano")
+        llm_transformer = LLMGraphTransformer(llm=llm)
+        
+        documents = get_doc_from_json(json_path)
+        graph_documents = await llm_transformer.aconvert_to_graph_documents(documents)
+        self.graph.add_graph_documents(graph_documents)
 
-    def generate_schema():
-        pass
+def get_doc_from_json(json_path: str):
+
+    with open(json_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    chunk_docs = list(
+        map(lambda chunk: Document(page_content=(chunk["content"])), data["chunks"][:100])
+    )
+
+    return chunk_docs
 
 if __name__ == "__main__":
 
@@ -27,3 +57,4 @@ if __name__ == "__main__":
         password=os.getenv("DB_PASSWORD"),
         openai_api_key=os.getenv("OPENAI_API_KEY")
     )
+    asyncio.run(db.create_graph("./data/biology_form_4.json"))
